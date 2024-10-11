8659bef8ac5127805cd91e12cba12f32e8b08e8f
mendozalz <mendoza124302@gmail.com>
2024-10-11 17:24:29 -0500

    Modificando al arquitectura de carpetas para trabajar con css.modules y arrancando con el hero

---
+++ src/components/Hero/HeroPresentation.tsx
@@ -0,0 +1,28 @@
+import styles from './hero.module.css'
+import { useRef } from 'react';
+
+interface Props {
+  video: string;
+  videoMovil: string;
+  isChanging: boolean;
+  text: string;
+}
+
+const HeroPresentation = ({video, videoMovil, isChanging, text}:Props) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  return (
+    <header className={styles.hero}>
+  <video className={styles.videoHero} ref={videoRef} src={video} autoPlay muted/>
+  <video className={styles.videoHeroMovil} ref={videoRef} src={videoMovil} autoPlay muted/>
+  <div className={styles.titulo}>
+    <h1 className={`${styles['fade-text']} ${isChanging ? styles['fade-out'] : styles['fade-in']} ${styles['hero-text']}`}>
+      {text}
+    </h1>
+      {/* <Formulario/> */}
+      </div>
+    </header>
+  )
+}
+
+export default HeroPresentation
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef, useState } from "react";
+import video from "./public/assets/Introfilm-HD-KeyVisual.mp4";
+import videoMovil from "./public/assets/Introfilm-HD-KeyVisual-mobile.mp4"
+import HeroPresentation from "./HeroPresentation";
+
+const Hero = () => {
+  const [text, setText] = useState(
+    "Aqua verde Condominio Campestre es el lugar perfecto para tu hogar"
+  );
+  const [isChanging, setIsChanging] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsChanging(true);
+      setTimeout(() => {
+        setText(
+          "Experimenta la exclusividad de un proyecto con lago privado, a pocos minutos de Medellín"
+        );
+        setIsChanging(false);
+      }, 950); // Espera a que se desvanezca antes de cambiar el texto
+    }, 2600);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // UseEffect para repetir el video entre espacios de tiempo
+
+  useEffect(() => {
+    const handleVideoEnded = () => {
+      setTimeout(() => {
+        if (videoRef.current) {
+          videoRef.current.play();
+        }
+      }, 10000); // Retraso de 10 segundos antes de repetir el video
+    };
+
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      videoElement.addEventListener("ended", handleVideoEnded);
+    }
+
+    return () => {
+      if (videoElement) {
+        videoElement.removeEventListener("ended", handleVideoEnded);
+      }
+    };
+  }, []);
+
+  return <><HeroPresentation video={video} videoMovil={videoMovil} isChanging={isChanging} text={text}/></>;
+};
+
+export default Hero;
